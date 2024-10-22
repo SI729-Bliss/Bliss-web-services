45a3530c548a4949f52dabfca549b086d6ad4fe2
dars2002 <u20191e831@upc.edu.pe>
2024-10-22 17:06:09 -0500

    feat:shared initial configuration

--- src/main/java/com/beautyservices/bliss/BlissApplication.java
+++ src/main/java/com/beautyservices/bliss/BlissApplication.java
@@ -2,7 +2,9 @@ package com.beautyservices.bliss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlissApplication {
 
