7ddcea94273ef72cabb5c1556edf06c279696313
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-26 21:21:00 -0500

    refactor: set query services

--- src/main/java/com/beautyservices/bliss/profilemanagement/domain/services/SpecialistQueryService.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/domain/services/SpecialistQueryService.java
@@ -1,4 +1,14 @@
 package com.beautyservices.bliss.profilemanagement.domain.services;
 
-public class SpecialistQueryService {
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Specialist;
+import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetAllSpecialistsQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface SpecialistQueryService {
+    List<Specialist> handle(GetAllSpecialistsQuery query);
 }
+
+
