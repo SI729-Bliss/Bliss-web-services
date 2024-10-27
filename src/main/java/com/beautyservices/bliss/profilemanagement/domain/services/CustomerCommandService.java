3e2c10c36c09d0035300476f160db1d1e030fb7c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-26 21:16:25 -0500

    refactor: set command services

---
+++ src/main/java/com/beautyservices/bliss/profilemanagement/domain/services/CustomerCommandService.java
@@ -0,0 +1,13 @@
+package com.beautyservices.bliss.profilemanagement.domain.services;
+
+
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Customer;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCompanyCommand;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCustomerCommand;
+
+import java.util.Optional;
+
+public interface CustomerCommandService {
+    Optional<Customer> handle(UpdateCustomerCommand command);
+
+}
