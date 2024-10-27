af2f2da36b84d53ee9fe3efcdef09aa78e9b4b52
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-27 00:26:25 -0500

    fix: add service bean for accurate running

--- src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/queryservices/ServicessQueryServiceImpl.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/queryservices/ServicessQueryServiceImpl.java
@@ -4,9 +4,11 @@ import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Servic
 import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetAllServicesQuery;
 import com.beautyservices.bliss.profilemanagement.domain.services.ServicesQueryService;
 import com.beautyservices.bliss.profilemanagement.infrastructure.persistence.jpa.repositories.ServiceRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ServicessQueryServiceImpl implements ServicesQueryService {
 
     private final ServiceRepository serviceRepository;
--- src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/queryservices/SpecialistQueryServiceImpl.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/queryservices/SpecialistQueryServiceImpl.java
@@ -4,9 +4,11 @@ import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Specia
 import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetAllSpecialistsQuery;
 import com.beautyservices.bliss.profilemanagement.domain.services.SpecialistQueryService;
 import com.beautyservices.bliss.profilemanagement.infrastructure.persistence.jpa.repositories.SpecialistRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SpecialistQueryServiceImpl implements SpecialistQueryService {
 
     private final SpecialistRepository specialistRepository;
