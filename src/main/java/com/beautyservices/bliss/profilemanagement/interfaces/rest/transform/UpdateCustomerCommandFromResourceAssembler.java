50481815fc6e53fb56cd2695424f3eba70930d7b
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-27 00:30:24 -0500

    fix: renamed function called in a wrong file

--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/transform/UpdateCustomerCommandFromResourceAssembler.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/transform/UpdateCustomerCommandFromResourceAssembler.java
@@ -1,10 +1,10 @@
 package com.beautyservices.bliss.profilemanagement.interfaces.rest.transform;
 
-import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCompanyCommand;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCustomerCommand;
 import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CustomerResource;
 
 public class UpdateCustomerCommandFromResourceAssembler {
-    public static UpdateCompanyCommand toCommandFromResource(Long id, CustomerResource resource) {
-        return new UpdateCompanyCommand(id, resource.name(), resource.email(), resource.phoneNumber(), resource.address());
+    public static UpdateCustomerCommand toCommandFromResource(Long id, CustomerResource resource) {
+        return new UpdateCustomerCommand(id, resource.name(), resource.email(), resource.phoneNumber(), resource.address());
     }
 }
