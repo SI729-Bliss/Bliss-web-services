8bf8bb185d7258c41cae32164f2437cf7f79ce81
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-26 21:11:49 -0500

    chore: set user not found exception

--- src/main/java/com/beautyservices/bliss/profilemanagement/domain/exceptions/UserNotFoundException.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/domain/exceptions/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package com.beautyservices.bliss.profilemanagement.domain.exceptions;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Long userId) {
+        super("User with ID " + userId + " was not found.");
+    }
 }
