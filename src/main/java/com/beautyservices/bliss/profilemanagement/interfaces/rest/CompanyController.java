d4e2ac4f5939f9beb7d8939db311fa4b773c30ca
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-26 21:23:32 -0500

    refactor: set controllers

--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CompanyController.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CompanyController.java
@@ -1,4 +1,47 @@
 package com.beautyservices.bliss.profilemanagement.interfaces.rest;
 
+import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetCompanyByIdQuery;
+import com.beautyservices.bliss.profilemanagement.domain.services.CompanyCommandService;
+import com.beautyservices.bliss.profilemanagement.domain.services.CompanyQueryService;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CompanyResource;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.CompanyResourceFromEntityAssembler;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.UpdateCompanyCommandFromResourceAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT})
+@RestController
+@RequestMapping(value="/api/v1/companies", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Companies", description = "Company Management Endpoints")
 public class CompanyController {
+    private final CompanyQueryService companyQueryService;
+    private final CompanyCommandService companyCommandService;
+
+    public CompanyController(CompanyQueryService companyQueryService, CompanyCommandService companyCommandService) {
+        this.companyQueryService = companyQueryService;
+        this.companyCommandService = companyCommandService;
+    }
+
+    @GetMapping("/{companyId}")
+    public ResponseEntity<CompanyResource> getCompanyById(@PathVariable Long companyId) {
+        var getCompanyByIdQuery = new GetCompanyByIdQuery(companyId);
+        var optionalCompany = this.companyQueryService.handle(getCompanyByIdQuery);
+        if (optionalCompany.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var companyResource = CompanyResourceFromEntityAssembler.toResourceFromEntity(optionalCompany.get());
+        return ResponseEntity.ok(companyResource);
+    }
+    @PutMapping("/{companyId}")
+    public ResponseEntity<CompanyResource> updateCompany(@PathVariable Long companyId, @RequestBody CompanyResource resource) {
+        var updateCompanyCommand = UpdateCompanyCommandFromResourceAssembler.toCommandFromResource(companyId, resource);
+        var optionalCompany = this.companyCommandService.handle(updateCompanyCommand);
+        if (optionalCompany.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        var companyResource = CompanyResourceFromEntityAssembler.toResourceFromEntity(optionalCompany.get());
+        return ResponseEntity.ok(companyResource);
+    }
 }
--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CustomerController.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CustomerController.java
@@ -1,4 +1,55 @@
 package com.beautyservices.bliss.profilemanagement.interfaces.rest;
 
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCompanyCommand;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCustomerCommand;
+import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetCustomerByIdQuery;
+import com.beautyservices.bliss.profilemanagement.domain.services.CustomerCommandService;
+import com.beautyservices.bliss.profilemanagement.domain.services.CustomerQueryService;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CustomerResource;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.CompanyResourceFromEntityAssembler;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.CustomerResourceFromEntityAssembler;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.UpdateCustomerCommandFromResourceAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.PUT})
+
+@RestController
+@RequestMapping(value="/api/customers", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Customers", description = "Customer Management Endpoints")
 public class CustomerController {
+
+    private final CustomerQueryService customerQueryService;
+    private final CustomerCommandService customerCommandService;
+
+    public CustomerController(CustomerQueryService customerQueryService, CustomerCommandService customerCommandService) {
+        this.customerQueryService = customerQueryService;
+        this.customerCommandService = customerCommandService;
+    }
+
+
+    @GetMapping("/{Customerid}")
+    public ResponseEntity<CustomerResource> getCustomerById(@PathVariable Long Customerid) {
+        var getCustomerByIdQuery = new GetCustomerByIdQuery(Customerid);
+        var optionalCustomer = customerQueryService.handle(getCustomerByIdQuery);
+        if (optionalCustomer.isEmpty())
+            return ResponseEntity.badRequest().build();
+        var customerResource = CustomerResourceFromEntityAssembler.toResourceFromEntity(optionalCustomer.get());
+        return ResponseEntity.ok(customerResource);
+
+    }
+
+    @PutMapping("/{Customerid}")
+    public ResponseEntity<CustomerResource> updateCustomer(@PathVariable Long id, @RequestBody CustomerResource resource) {
+        var updateCustomerCommand = UpdateCustomerCommandFromResourceAssembler.toCommandFromResource(id, resource);
+        var optionalCustomer = this.customerCommandService.handle(updateCustomerCommand);
+        if (optionalCustomer.isEmpty())
+            return ResponseEntity.badRequest().build();
+        var customerResource = CustomerResourceFromEntityAssembler.toResourceFromEntity(optionalCustomer.get());
+        return ResponseEntity.ok(customerResource);
+    }
+
+
 }
