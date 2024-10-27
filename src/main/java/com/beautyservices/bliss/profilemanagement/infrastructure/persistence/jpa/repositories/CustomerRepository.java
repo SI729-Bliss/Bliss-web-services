d5a895ed40a1e5d5285ddcef1fcf078a00f24ef7
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-26 21:20:42 -0500

    refactor: set repositories

---
+++ src/main/java/com/beautyservices/bliss/profilemanagement/infrastructure/persistence/jpa/repositories/CustomerRepository.java
@@ -0,0 +1,18 @@
+package com.beautyservices.bliss.profilemanagement.infrastructure.persistence.jpa.repositories;
+
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Company;
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Customer;
+import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetCustomerByIdQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> handle(GetCustomerByIdQuery query);
+
+    boolean existsById(Long id);
+    Optional<Customer> findById(Long id);
+
+}
--- src/main/java/com/beautyservices/bliss/profilemanagement/infrastructure/persistence/jpa/repositories/SpecialistRepository.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/infrastructure/persistence/jpa/repositories/SpecialistRepository.java
@@ -1,4 +1,14 @@
 package com.beautyservices.bliss.profilemanagement.infrastructure.persistence.jpa.repositories;
 
-public class SpecialistRepository {
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Servicess;
+import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Specialist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
+    boolean existsById(Long id);
+    Optional<Specialist> findById(Long id);
 }
