c8081afbc6e68f8b760f47d195954bdf86d076b5
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-27 00:23:53 -0500

    refactor: renamed parameter wrong called

--- src/main/java/com/beautyservices/bliss/profilemanagement/domain/model/valueobjects/Email.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/domain/model/valueobjects/Email.java
@@ -5,15 +5,15 @@ import jakarta.persistence.Embeddable;
 import java.util.regex.Pattern;
 
 @Embeddable
-public record Email(String value) {
+public record Email(String email) {
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
 
     public Email {
-        if (value == null || value.isBlank()) {
+        if (email == null || email.isBlank()) {
             throw new IllegalArgumentException("Email cannot be null or blank");
         }
-        if (!EMAIL_PATTERN.matcher(value).matches()) {
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
             throw new IllegalArgumentException("Invalid email format");
         }
     }
