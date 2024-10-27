2b8ee612c3456b6de2baa945b437eb8f5c6f17ef
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-27 00:23:34 -0500

    refactor: renamed parameter wrong called

--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CustomerController.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/CustomerController.java
@@ -1,12 +1,9 @@
 package com.beautyservices.bliss.profilemanagement.interfaces.rest;
 
-import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCompanyCommand;
-import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCustomerCommand;
 import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetCustomerByIdQuery;
 import com.beautyservices.bliss.profilemanagement.domain.services.CustomerCommandService;
 import com.beautyservices.bliss.profilemanagement.domain.services.CustomerQueryService;
 import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CustomerResource;
-import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.CompanyResourceFromEntityAssembler;
 import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.CustomerResourceFromEntityAssembler;
 import com.beautyservices.bliss.profilemanagement.interfaces.rest.transform.UpdateCustomerCommandFromResourceAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.PUT})
-
 @RestController
 @RequestMapping(value="/api/customers", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Customers", description = "Customer Management Endpoints")
@@ -30,9 +26,9 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/{Customerid}")
-    public ResponseEntity<CustomerResource> getCustomerById(@PathVariable Long Customerid) {
-        var getCustomerByIdQuery = new GetCustomerByIdQuery(Customerid);
+    @GetMapping("/{customerid}")
+    public ResponseEntity<CustomerResource> getCustomerById(@PathVariable Long customerid) {
+        var getCustomerByIdQuery = new GetCustomerByIdQuery(customerid);
         var optionalCustomer = customerQueryService.handle(getCustomerByIdQuery);
         if (optionalCustomer.isEmpty())
             return ResponseEntity.badRequest().build();
@@ -41,15 +37,17 @@ public class CustomerController {
 
     }
 
-    @PutMapping("/{Customerid}")
-    public ResponseEntity<CustomerResource> updateCustomer(@PathVariable Long id, @RequestBody CustomerResource resource) {
-        var updateCustomerCommand = UpdateCustomerCommandFromResourceAssembler.toCommandFromResource(id, resource);
+    @PutMapping("/{customerId}")
+    public ResponseEntity<CustomerResource> updateCustomer(@PathVariable Long customerId, @RequestBody CustomerResource resource) {
+        var updateCustomerCommand = UpdateCustomerCommandFromResourceAssembler.toCommandFromResource(customerId, resource);
         var optionalCustomer = this.customerCommandService.handle(updateCustomerCommand);
         if (optionalCustomer.isEmpty())
             return ResponseEntity.badRequest().build();
         var customerResource = CustomerResourceFromEntityAssembler.toResourceFromEntity(optionalCustomer.get());
         return ResponseEntity.ok(customerResource);
     }
+
+
 
 
 }
