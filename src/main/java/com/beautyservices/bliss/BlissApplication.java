be8f22751be356d22460f4fabfa0f7223df227b4
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-05 00:39:12 -0500

    fix: set config for allow requests

--- src/main/java/com/beautyservices/bliss/BlissApplication.java
+++ src/main/java/com/beautyservices/bliss/BlissApplication.java
@@ -11,26 +11,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlissApplication {
-	
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-	
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlissApplication.class, args);
 	}
+
+	
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+		    return new WebMvcConfigurer() {
+		        @Override
+		        public void addCorsMappings(CorsRegistry registry) {
+		            registry.addMapping("/**")
+		                    .allowedOrigins("https://exquisite-possibility-production.up.railway.app") // Cambia por tu dominio
+		                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+		                    .allowedHeaders("Authorization", "Content-Type", "Accept")
+		                    .allowCredentials(true);
+		        }
+		    };
+		}
+
+
 	
 
 }
