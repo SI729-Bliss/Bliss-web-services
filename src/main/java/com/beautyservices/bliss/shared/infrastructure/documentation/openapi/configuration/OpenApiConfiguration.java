eb15111de45e227dadb2da4be657cfd6b47ba27b
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-16 03:34:14 -0500

    chore: set openAPI config

--- src/main/java/com/beautyservices/bliss/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/beautyservices/bliss/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.beautyservices.bliss.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +26,21 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("Bliss Platform Documentation")
                         .url("https://github.com/SI729-Bliss/Bliss-web-services"));
+
+        // Add security scheme
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        // Return OpenAPI configuration object with all the settings
         return openApi;
     }
 }
