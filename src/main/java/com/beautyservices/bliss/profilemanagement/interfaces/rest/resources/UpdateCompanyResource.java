3c58b70196b4af9991a8dce1ac9a1114e130db5c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-17 05:13:34 -0500

    feat: add rating to company resource

--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/resources/UpdateCompanyResource.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/resources/UpdateCompanyResource.java
@@ -4,5 +4,5 @@ import com.beautyservices.bliss.profilemanagement.domain.model.valueobjects.Addr
 import com.beautyservices.bliss.profilemanagement.domain.model.valueobjects.Email;
 import com.beautyservices.bliss.profilemanagement.domain.model.valueobjects.PhoneNumber;
 
-public record UpdateCompanyResource(String name, Email email, Address address, PhoneNumber phoneNumber) {
+public record UpdateCompanyResource(String name, Email email, Address address, PhoneNumber phoneNumber, double rating) {
 }
