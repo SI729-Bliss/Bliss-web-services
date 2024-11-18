207dfbc8807341d952fee77783c7236cd659b0aa
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-17 20:08:46 -0500

    feat: add iam outbound service

---
+++ src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/outboundservices/acl/ExternalIamService.java
@@ -0,0 +1,27 @@
+package com.beautyservices.bliss.profilemanagement.application.internal.outboundservices.acl;
+
+import com.beautyservices.bliss.iam.domain.model.aggregates.User;
+import com.beautyservices.bliss.iam.interfaces.acl.IamContextFacade;
+import com.beautyservices.bliss.profilemanagement.domain.model.valueobjects.Email;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ExternalIamService {
+
+    private final IamContextFacade iamContextFacade;
+
+    public ExternalIamService(IamContextFacade iamContextFacade) {
+        this.iamContextFacade = iamContextFacade;
+    }
+
+    public Optional<User> fetchUserIdByUsername(String email) {
+        String username = String.valueOf(iamContextFacade.fetchUserIdByUsername(email));
+        if (username.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of( new User(username));
+    }
+
+}
