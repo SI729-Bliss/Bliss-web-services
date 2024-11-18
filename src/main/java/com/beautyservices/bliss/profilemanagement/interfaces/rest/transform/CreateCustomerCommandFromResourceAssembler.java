8e24d47ecafe6b7d36bd8eac5f6ad169abdf4ba1
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-18 04:16:36 -0500

    fix(profile-management): add post and get endpoints for customers and companies

--- src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/transform/CreateCustomerCommandFromResourceAssembler.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/interfaces/rest/transform/CreateCustomerCommandFromResourceAssembler.java
@@ -1,4 +1,14 @@
 package com.beautyservices.bliss.profilemanagement.interfaces.rest.transform;
 
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.CreateCompanyCommand;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.CreateCustomerCommand;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CreateCompanyResource;
+import com.beautyservices.bliss.profilemanagement.interfaces.rest.resources.CreateCustomerResource;
+
 public class CreateCustomerCommandFromResourceAssembler {
+    public static CreateCustomerCommand toCommand(CreateCustomerResource resource) {
+        return new CreateCustomerCommand(
+                resource.id(),
+                resource.name(), resource.email(), resource.address(), resource.phoneNumber());
+    }
 }
