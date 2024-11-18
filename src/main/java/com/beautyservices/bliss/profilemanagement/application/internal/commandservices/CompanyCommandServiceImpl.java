0eedfde475296917fb634e670e2a0a19fa699d27
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-17 21:43:14 -0500

    feat(profile-management): implements command services

--- src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/commandservices/CompanyCommandServiceImpl.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/application/internal/commandservices/CompanyCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.beautyservices.bliss.profilemanagement.application.internal.commands
 
 import com.beautyservices.bliss.profilemanagement.application.internal.outboundservices.acl.ExternalIamService;
 import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Company;
+import com.beautyservices.bliss.profilemanagement.domain.model.commands.CreateCompanyCommand;
 import com.beautyservices.bliss.profilemanagement.domain.model.commands.UpdateCompanyCommand;
 import com.beautyservices.bliss.profilemanagement.domain.services.CompanyCommandService;
 import com.beautyservices.bliss.profilemanagement.infrastructure.persistence.jpa.repositories.CompanyRepository;
@@ -47,5 +48,30 @@ public class CompanyCommandServiceImpl implements CompanyCommandService {
         } catch (Exception e) {
             throw new IllegalArgumentException("Error while updating company profile: " + e.getMessage());
         }
+    }
+
+    @Override
+    public Optional<Company> handle(CreateCompanyCommand command) {
+        // Validate if profile already exists with the same name
+        var optionalUsername = this.externalIamService.fetchUserIdByUsername(command.email());
+        if (optionalUsername.isPresent()) {
+            this.companyRepository.findById(optionalUsername.get().getId()).ifPresent(student -> {
+                throw new IllegalArgumentException("Company already exists");
+            });
+        }
+
+        // Create profile
+        optionalUsername = this.externalIamService.createUsername(command.email(), command.password());
+        if (optionalUsername.isEmpty()) {
+            throw new IllegalArgumentException("Unable to create profile");
+        }
+        var company = new Company(command);
+        try {
+            this.companyRepository.save(company);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while saving course assign: " + e.getMessage());
+        }
+
+        return company.getId();
     }
 }
