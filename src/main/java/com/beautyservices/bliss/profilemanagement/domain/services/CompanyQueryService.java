0b8c70d942c7bae8152eaf531046ef81dbd19be5
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-17 21:42:47 -0500

    feat(profile-management): add command n query services

--- src/main/java/com/beautyservices/bliss/profilemanagement/domain/services/CompanyQueryService.java
+++ src/main/java/com/beautyservices/bliss/profilemanagement/domain/services/CompanyQueryService.java
@@ -1,10 +1,13 @@
 package com.beautyservices.bliss.profilemanagement.domain.services;
 
 import com.beautyservices.bliss.profilemanagement.domain.model.aggregates.Company;
+import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetAllCompaniesQuery;
 import com.beautyservices.bliss.profilemanagement.domain.model.queries.GetCompanyByIdQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CompanyQueryService {
     Optional<Company> handle(GetCompanyByIdQuery query);
+    List<Company> handle(GetAllCompaniesQuery query);
 }
