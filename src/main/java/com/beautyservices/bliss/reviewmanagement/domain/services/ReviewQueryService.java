16ccad5f4aba53ccefb8bbf514bfa9cf8d307267
ElviaRV <elviamarcela14@gmail.com>
2024-11-19 09:13:02 -0500

    fix: handlers from repository fixed

--- src/main/java/com/beautyservices/bliss/reviewmanagement/domain/services/ReviewQueryService.java
+++ src/main/java/com/beautyservices/bliss/reviewmanagement/domain/services/ReviewQueryService.java
@@ -1,15 +1,18 @@
 package com.beautyservices.bliss.reviewmanagement.domain.services;
 
 import com.beautyservices.bliss.reviewmanagement.domain.model.aggregates.Review;
+import com.beautyservices.bliss.reviewmanagement.domain.model.queries.GetReviewByIdQuery;
+import com.beautyservices.bliss.reviewmanagement.domain.model.queries.GetReviewsByCompanyIdQuery;
 import com.beautyservices.bliss.reviewmanagement.domain.model.queries.GetReviewsByReservationIdQuery;
+import com.beautyservices.bliss.reviewmanagement.domain.model.queries.GetReviewsByUserIdQuery;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReviewQueryService {
-    List<Review> getReviewsByCompanyId(Long companyId);
-    List<Review> getReviewsByUserId(Long userId);
-    Optional<Review> getReviewById(Long id);
-    List<Review> getReviewsByReservationId(GetReviewsByReservationIdQuery query);
+    List<Review> handle (GetReviewsByCompanyIdQuery query);
+    List<Review> handle (GetReviewsByUserIdQuery query);
+    Optional<Review> handle (GetReviewByIdQuery query);
+    List<Review> handle (GetReviewsByReservationIdQuery query);
 
 }
