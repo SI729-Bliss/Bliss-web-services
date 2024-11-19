df54b18dec5401fb85d727aef4bea9c4889f0781
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-11-19 06:19:57 -0500

    chore: set deploy config

--- src/main/java/com/beautyservices/bliss/BlissApplication.java
+++ src/main/java/com/beautyservices/bliss/BlissApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -22,13 +29,24 @@ public class BlissApplication {
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
 			registry.addMapping("/**") // Permite todas las rutas
-					.allowedOrigins("http://localhost:4200") // Permite el origen de tu frontend
+					.allowedOrigins("*") // Permite el origen de tu frontend
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
 					.allowedHeaders("*") // Permite todos los encabezados
 					.allowCredentials(true); // Permite el uso de credenciales, si es necesario
 		}
 	}
-
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// Don't do this in production, use a proper list  of allowed origins
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 
 
 }
